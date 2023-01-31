10f9663e1d5358c566fa04d80ed8c3da06d5cb44
Austin Papritz <austin@papritz.dev>
2023-01-31 14:19:45 -0800

    quartery earnings chart with theme added to Carts.js

--- src/components/Charts/Charts.js
+++ src/components/Charts/Charts.js
@@ -1,6 +1,24 @@
 import React from 'react';
 import './Charts.css';
 
+import { VictoryBar, VictoryChart, VictoryAxis, VictoryTheme } from 'victory';
+
+const data = [
+  { quarter: 1, earnings: 13000 },
+  { quarter: 2, earnings: 16500 },
+  { quarter: 3, earnings: 14250 },
+  { quarter: 4, earnings: 19000 },
+];
+
 export default function Chart() {
-  return <div>Chart</div>;
+  return (
+    <VictoryChart theme={VictoryTheme.material} domainPadding={20}>
+      <VictoryAxis
+        tickValues={[1, 2, 3, 4]}
+        tickFormat={['Quarter 1', 'Quarter 2', 'Quarter 3', 'Quarter 4']}
+      ></VictoryAxis>
+      <VictoryAxis dependentAxis tickFormat={(x) => `$${x / 1000}k`}></VictoryAxis>
+      <VictoryBar data={data} x="quarter" y="earnings" />
+    </VictoryChart>
+  );
 }

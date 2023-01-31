db681148796ef044c827b2887aa2de18033a5492
Austin Papritz <austin@papritz.dev>
2023-01-31 12:02:46 -0800

    changed path of components folder

--- src/services/components/TableComp/TableComp.js
+++ src/services/components/TableComp/TableComp.js
@@ -1,50 +1,91 @@
 import React from 'react';
 import './TableComp.css';
 
+import fakeData from '../../../data/data.js';
+
 import { Table, Column, HeaderCell, Cell } from 'rsuite-table';
 import 'rsuite-table/lib/less/index.less'; // or 'rsuite-table/dist/css/rsuite-table.css'
 
-const dataList = [
-  { id: 1, name: 'a', email: 'a@email.com', avartar: '...' },
-  { id: 2, name: 'b', email: 'b@email.com', avartar: '...' },
-  { id: 3, name: 'c', email: 'c@email.com', avartar: '...' },
-];
-
-const ImageCell = ({ rowData, dataKey, ...rest }) => (
-  <Cell {...rest}>
-    <img src={rowData[dataKey]} width="50" />
-  </Cell>
-);
-
 export default function TableComp() {
+  // data is empty initially
+  const [data, setData] = React.useState([]);
+
+  React.useEffect(() => {
+    // We load data after first render
+    setData(fakeData);
+  }, []);
+
+  const handleScroll = (x, y) => {
+    // This should print 200 under normal conditions
+    console.log(data.length);
+  };
+
   return (
-    <>
-      <p>test</p>
-      <Table data={dataList}>
-        <Column width={100} sortable fixed resizable>
-          <HeaderCell>ID</HeaderCell>
-          <Cell dataKey="id" />
-        </Column>
+    <Table
+      height={400}
+      data={data}
+      onScroll={handleScroll}
+      cellBordered
+      onRowClick={(data) => {
+        console.log(data);
+      }}
+    >
+      <Column width={70} align="center" fixed>
+        <HeaderCell>Id</HeaderCell>
+        <Cell dataKey="id" />
+      </Column>
 
-        <Column width={100} sortable resizable>
-          <HeaderCell>Name</HeaderCell>
-          <Cell dataKey="name" />
-        </Column>
+      <Column width={130} fixed>
+        <HeaderCell>First Name</HeaderCell>
+        <Cell dataKey="firstName" />
+      </Column>
 
-        <Column width={100} sortable resizable>
-          <HeaderCell>Email</HeaderCell>
-          <Cell>
-            {(rowData, rowIndex) => {
-              return <a href={`mailto:${rowData.email}`}>{rowData.email}</a>;
-            }}
-          </Cell>
-        </Column>
+      <Column width={130}>
+        <HeaderCell>Last Name</HeaderCell>
+        <Cell dataKey="lastName" />
+      </Column>
 
-        <Column width={100} resizable>
-          <HeaderCell>Avartar</HeaderCell>
-          <ImageCell dataKey="avartar" />
-        </Column>
-      </Table>
-    </>
+      <Column width={200}>
+        <HeaderCell>City</HeaderCell>
+        <Cell dataKey="city" />
+      </Column>
+
+      <Column width={200}>
+        <HeaderCell>Street</HeaderCell>
+        <Cell dataKey="street" />
+      </Column>
+
+      <Column width={200}>
+        <HeaderCell>Company</HeaderCell>
+        <Cell dataKey="company" />
+      </Column>
+
+      <Column width={200}>
+        <HeaderCell>Email</HeaderCell>
+        <Cell dataKey="email" />
+      </Column>
+
+      <Column width={200}>
+        <HeaderCell>Email</HeaderCell>
+        <Cell dataKey="email" />
+      </Column>
+
+      <Column width={200} fixed="right">
+        <HeaderCell>Action</HeaderCell>
+
+        <Cell>
+          {(rowData) => {
+            function handleAction() {
+              alert(`id:${rowData.id}`);
+            }
+            return (
+              <span>
+                <a onClick={handleAction}> Edit </a> | <a onClick={handleAction}> Remove </a>
+              </span>
+            );
+          }}
+        </Cell>
+      </Column>
+    </Table>
   );
 }

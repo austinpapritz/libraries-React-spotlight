aea9fcabe75b14f14bee84a8decd88382bb73cf1
Austin Papritz <austin@papritz.dev>
2023-01-31 10:59:16 -0800

    removed react-boostrap-table import in Tables.js

--- src/services/components/Table/Table.js
+++ src/services/components/Table/Table.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import './Table.css';
 
-import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
-
 export default function Table() {
   return <div>Table</div>;
 }
